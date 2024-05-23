b806773da33283a3d67b174c74de50fa7ab3e887
fabian <fabianmuthrif@gmail.com>
2024-05-23 22:34:21 +0700

    add login_object_orangeHRM add login_step_orangeHRM

---
+++ src/test/java/object/login_object_orangeHRM.java
@@ -0,0 +1,17 @@
+package object;
+
+import org.openqa.selenium.By;
+
+public class login_object_orangeHRM {
+    By txtUsername = By.xpath("//*[@placeholder=\"Username\"]");
+    public By getTxtUsername(){ return txtUsername; }
+
+    By txtPassword = By.xpath("//*[@type=\"password\"]");
+    public By getTxtPassword(){ return txtPassword; }
+
+    By btnLogin = By.xpath("//*[@class=\"oxd-button oxd-button--medium oxd-button--main orangehrm-login-button\"]");
+    public By getBtnLogin(){ return btnLogin; }
+
+    By txtDashboard = By.xpath("//*[@contains(text(), 'Dashboard']");
+    public By getTxtDashboard(){ return txtDashboard; }
+}
---
+++ src/test/java/step/login_step_orangeHRM.java
@@ -0,0 +1,39 @@
+package step;
+
+import config.env;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import object.login_object_orangeHRM;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class login_step_orangeHRM extends env {
+    login_object_orangeHRM elementLogin = new login_object_orangeHRM();
+    @Given("User is in login page")
+    public void user_is_in_login_page() {
+        wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.elementToBeClickable(elementLogin.getBtnLogin()));
+        driver.findElement(elementLogin.getTxtUsername()).isDisplayed();
+        driver.findElement(elementLogin.getTxtPassword()).isDisplayed();
+        driver.findElement(elementLogin.getBtnLogin()).isDisplayed();
+    }
+    @When("User input username")
+    public void user_input_username() {
+        driver.findElement(elementLogin.getTxtUsername()).sendKeys("Admin");
+    }
+    @And("User input password")
+    public void user_input_password() {
+        driver.findElement(elementLogin.getTxtPassword()).sendKeys("admin123");
+    }
+    @And("User click login")
+    public void user_click_login() {
+        driver.findElement(elementLogin.getBtnLogin()).click();
+    }
+    @Then("user redirect to dashboard page")
+    public void user_redirect_to_dashboard_page() {
+        wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(elementLogin.getTxtDashboard())).isDisplayed();
+    }
+}
